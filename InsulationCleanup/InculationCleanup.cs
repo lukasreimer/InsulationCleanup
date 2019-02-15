efbf4debe693aaac9a8b218f70f62d26baa5f45e
Lukas Reimer <lukas.reimer@burohappold.com>
2019-02-15 10:25:51 +0100

    Added Stub Implementation of a Revit External Command

---
+++ InsulationCleanup/InculationCleanup.cs
@@ -0,0 +1,23 @@
+﻿// using System;
+// using System.Collections.Generic;
+// using System.Linq;
+// using System.Text;
+// using System.Threading.Tasks;
+
+// using Autodesk.Revit.ApplicationServices;
+// using Autodesk.Revit.Attributes;
+using Autodesk.Revit.DB;
+using Autodesk.Revit.UI;
+// using Autodesk.Revit.UI.Selection;
+
+namespace InsulationCleanup
+{
+    public class ChangeToHostWorkset : IExternalCommand
+    {
+        public Result Execute(ExternalCommandData commandData, ref string message, ElementSet elements)
+        {
+            TaskDialog.Show("Insulation Cleanup Addin", "Changing Insulation to Host Workset...");
+            return Result.Succeeded;
+        }
+    }
+}
