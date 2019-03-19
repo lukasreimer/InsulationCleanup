c8df558f1fffb6ce9b1f801bd6d2ef13ac2c3a7b
Lukas Reimer <lukas.reimer@burohappold.com>
2019-03-19 20:05:59 +0100

    Extracted ShowReport Method

--- InsulationCleanup/InsulationCleanup.cs
+++ InsulationCleanup/InsulationCleanup.cs
@@ -18,8 +18,6 @@ namespace InsulationCleanup
     [Regeneration(RegenerationOption.Manual)]
     public class ChangeToHostWorkset : IExternalCommand
     {
-        private String title = "ChangeToHostWorkset Addin";
-
         public Result Execute(ExternalCommandData commandData, ref string message, ElementSet elements)
         {
             try
@@ -33,8 +31,8 @@ namespace InsulationCleanup
                 var collector = new FilteredElementCollector(doc);
                 IList<Element> insulationElements = collector.WherePasses(filter).WhereElementIsNotElementType().ToElements();
 
-                // Collect all rogue insulation elements (insulation workset is not host workset)
-                var rogueElements = new List<Tuple<PipeInsulation, Element>>();  // (rogue insulation object, target element object)
+                // Collect all rogue insulation elements (where insulation workset is not host workset)
+                var rogueElements = new List<Tuple<PipeInsulation, Element>>();  // (rogue insulation element, hosting element)
                 foreach (var element in insulationElements)
                 {
                     PipeInsulation insulationElement = element as PipeInsulation;
@@ -46,61 +44,93 @@ namespace InsulationCleanup
                     }
                 }
 
-                // Output a report of the identified rogue insulation elements
-                String prompt = $"There are {rogueElements.Count} rogue insulation elements in this document. ";
-                if (rogueElements.Count != 0)
+                // Show a report of the found rogue insulation elements
+                TaskDialogResult result = ShowReport(rogueElements);
+                TaskDialog.Show("Result", result.ToString());
+
+                if (result == TaskDialogResult.Yes)
                 {
-                    prompt += "They are:\n\n";
-                    foreach (var tuple in rogueElements)
+                    // Save the current workset for resetting later
+                    WorksetTable worksets = doc.GetWorksetTable();
+                    WorksetId initalWorksetId = worksets.GetActiveWorksetId();
+
+                    // Delete rogue insulation and create new insulation on correct host workset
+                    using (var trans = new Transaction(doc))
                     {
-                        PipeInsulation rogueElement = tuple.Item1;
-                        Element targetElement = tuple.Item2;
-                        prompt += $"rogue insulation: {rogueElement.Name} #{rogueElement.Id} @ workset: {rogueElement.WorksetId}, ";
-                        prompt += $"target element: {targetElement.Name} #{targetElement.Id} @ workset: {targetElement.WorksetId}\n";
+                        trans.Start("ChangeToHostWorkset");
+                        foreach (var tuple in rogueElements)
+                        {
+                            // Unpack tuple
+                            PipeInsulation rogueElement = tuple.Item1;
+                            Element targetElement = tuple.Item2;
+                            // Get data needed for PipeInsulation "constructor"
+                            var insulationTypeId = rogueElement.GetTypeId();
+                            var insulationThickness = rogueElement.Thickness;
+                            // Delete rogue insulation and create new insulation on the correct host
+                            doc.Delete(rogueElement.Id);
+                            try
+                            {
+                                worksets.SetActiveWorksetId(targetElement.WorksetId);
+                                PipeInsulation.Create(doc, targetElement.Id, insulationTypeId, insulationThickness);
+                            }
+                            catch (Exception)
+                            {
+                                // TODO: keep track of failed reincarnations and report after commiting the transaction
+                                // ...
+                            }
+                        }
+                        // Reset the active workset
+                        worksets.SetActiveWorksetId(initalWorksetId);
+                        trans.Commit();
+
+                        return Result.Succeeded;
                     }
                 }
-                TaskDialog.Show(this.title, prompt);
-
-                // Save the current workset for resetting later
-                WorksetTable worksets = doc.GetWorksetTable();
-                WorksetId initalWorksetId = worksets.GetActiveWorksetId();
-
-                // Delete rogue insulation and create new insulation on correct host workset
-                using (var trans = new Transaction(doc))
+                else  // not TaskDialogResult.Yes
                 {
-                    trans.Start("ChangeToHostWorkset");
-                    foreach (var tuple in rogueElements)
-                    {
-                        // Unpack tuple
-                        PipeInsulation rogueElement = tuple.Item1;
-                        Element targetElement = tuple.Item2;
-                        // Get data needed for PipeInsulation "constructor"
-                        var insulationTypeId = rogueElement.GetTypeId();
-                        var insulationThickness = rogueElement.Thickness;
-                        // Delete rogue insulation and create new insulation on the correct host
-                        doc.Delete(rogueElement.Id);
-                        try
-                        {
-                            worksets.SetActiveWorksetId(targetElement.WorksetId);
-                            PipeInsulation.Create(doc, targetElement.Id, insulationTypeId, insulationThickness);
-                        }
-                        catch (Exception)
-                        {
-                            // TODO: keep track of failed reincarnations and report after commiting the transaction
-                            // ...
-                        }
-                    }
-                    // Reset the active workset
-                    worksets.SetActiveWorksetId(initalWorksetId);
-                    trans.Commit();
+                    return Result.Cancelled;
                 }
             }
-            catch (Exception e)
+            catch (Exception e)  // any exception
             {
                 message = e.Message;
                 return Result.Failed;
             }
-            return Result.Succeeded;
+        }
+
+        private TaskDialogResult ShowReport(List<Tuple<PipeInsulation, Element>> rogueElements)
+        {
+            // setup text components for the dialog
+            string title = "Rogue Insulation";
+            string message = $"There are {rogueElements.Count} rogue insulation elements in this document.";
+            string content = "";
+            string footer = "Do you want to move the rogue insulation to the hosts worksets?";
+            // populate the detailed content part of the dialog
+            if (rogueElements.Count != 0)  // there are rogue elements
+            {
+                // iterate through all (rogue insulation, host element) tuples
+                foreach (var tuple in rogueElements)
+                {
+                    // unpack the tuple
+                    PipeInsulation rogueElement = tuple.Item1;
+                    Element targetElement = tuple.Item2;
+                    // create a report detail line
+                    // TODO: figure out the workset name
+                    content += $"insulation: {rogueElement.Name} #{rogueElement.Id} @ workset: {rogueElement.WorksetId}, ";
+                    content += $"hosted by: {targetElement.Name} #{targetElement.Id} @ workset: {targetElement.WorksetId}\n";
+                }
+            }
+            // configure and show dialog
+            TaskDialog dialog = new TaskDialog(title);
+            dialog.MainIcon = TaskDialogIcon.TaskDialogIconInformation;
+            dialog.MainInstruction = message;
+            dialog.ExpandedContent = content;
+            dialog.FooterText = footer;
+            dialog.AllowCancellation = false;
+            dialog.CommonButtons = TaskDialogCommonButtons.Yes | TaskDialogCommonButtons.No;
+            dialog.DefaultButton = TaskDialogResult.No;
+            TaskDialogResult result = dialog.Show();
+            return result;
         }
     }
 }
